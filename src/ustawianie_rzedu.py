e648492de3f81b6f7fc6acf2bf3366b1a0b50038
AniaWujek <aniawujek@gmail.com>
2016-05-24 00:04:21 +0200

    dziala ustawianie robotow w rzad

--- src/ustawianie_rzedu.py
+++ src/ustawianie_rzedu.py
@@ -96,6 +96,11 @@ time.sleep(0.5)
 desired_p = start_positions[robot_number]
 current_p = (robot.sensors["position"].read().pos[0], robot.sensors["position"].read().pos[1])
 
+print("I want to robot be in position:")
+print(desired_p)
+print("And orientation:")
+print(math.pi/2.0)
+
 #zadana orientacja i aktualna orientacja
 #uwaga, vrep jakos dziwnie to liczy - w vrepie orientacja jest w stopniach, a w pythonie
 #odbieramy ja w radianach
@@ -125,12 +130,10 @@ current_p = (robot.sensors["position"].read().pos[0], robot.sensors["position"].
 # jesli to glupie i uznacie, ze jednak vrep jest madrzejszy, to nie wahajcie sie tego zmienic :)
 
 
-# skoro mapa jest prostokatna i chcemy jechac takim rzedem, to kierunek mozna hardkodowac
-desired_o = math.pi/2.0;
+
 
 #interesuje nas 3 skladowa orientacji, bo nie bedziemy robic fikolkow ani beczek
 current_o = robot.sensors["orientation"].read().ori[2]
-print(current_o)
 if current_o < 0:
 	current_o = math.pi + (math.pi + current_o)
 
@@ -139,18 +142,66 @@ precision_p = 0.1
 precision_o = 0.05
 
 
+
+v_max_o = 5
+
+	
+
+#max. predkosc podczas obrotu
+v_max_p = 2
+
 #aktualna roznica miedzy pozycjami zadana a aktualna
-d_p = (desired_o[0] - current_o[0],desired_o[1] - current_o[1])
-v_max = 5
+d_p = (desired_p[0] - current_p[0],desired_p[1] - current_p[1])
+desired_o = math.atan2(-d_p[1], -d_p[0])+math.pi
+d_o = desired_o - current_o
 
 while abs(d_p[0]) > precision_p or abs(d_p[1]) > precision_p:
-	k = math.sqrt(math.pow(d_p[0], 2) + math.pow(d_p[1], 2))
-	v = min(v_max, k*v_max)
+	k_p = math.sqrt(math.pow(d_p[0], 2) + math.pow(d_p[1], 2))
+	v_p = min(v_max_p, k_p*v_max_p)
+	desired_o = math.atan2(-d_p[1], -d_p[0])+math.pi
+	k_o = min([abs(d_o), 2*math.pi-abs(d_o)])/(2*math.pi)
+	v_o = k_o*v_max_o
+	if d_o < -math.pi:
+		v_right = -v_o 
+		v_left = v_o
+	elif d_o < 0:
+		v_right = v_o 
+		v_left = -v_o
+	elif d_o < math.pi:
+		v_right = -v_o 
+		v_left = v_o
+	else:
+		v_right = v_o 
+		v_left = -v_o
+		
+	robot.motors["left"].velocity = v_right+v_p
+	robot.motors["right"].velocity = v_left+v_p
+	
+	#taki okres probkowania dziala
+	time.sleep(0.1)
+	current_o = robot.sensors["orientation"].read().ori[2]
+	if current_o < 0:
+		current_o = math.pi + (math.pi + current_o)
+	d_o = desired_o - current_o
+	current_p = (robot.sensors["position"].read().pos[0], robot.sensors["position"].read().pos[1])
+	d_p = (desired_p[0] - current_p[0],desired_p[1] - current_p[1])
+
+#stop robot
+robot.motors["left"].velocity = 0.0
+robot.motors["right"].velocity = 0.0
+
+#jak sie nie da tutaj sleepa to sie silniki nie zatrzymuja
+time.sleep(0.5)
 	
 
 
 
+# ***************************************
+# ustawienie tylko orientacji
+# ***************************************
 
+# skoro mapa jest prostokatna i chcemy jechac takim rzedem, to kierunek mozna hardkodowac
+desired_o = math.pi/2.0;
 
 #max. predkosc podczas obrotu
 v_max = 5
@@ -161,6 +212,7 @@ d_o = desired_o - current_o
 while abs(d_o) > precision_o:
 	#taki sobie regulator P - dziala
 	k = min([abs(d_o), 2*math.pi-abs(d_o)])/(2*math.pi)
+	#print(d_o)
 	v = k*v_max
 	#ify stad, ze chcemy obracac sie krotsza droga
 	# pewnie da sie to zrobic ladniej, ale tak dziala
@@ -197,10 +249,11 @@ time.sleep(0.5)
 current_o = robot.sensors["orientation"].read().ori[2]
 if current_o < 0:
 	current_o = math.pi + (math.pi + current_o)
-print("New orintation:")
-print("Desired orientation:")
-print(desired_o)
-print("Current orientation:")
+current_p = (robot.sensors["position"].read().pos[0], robot.sensors["position"].read().pos[1])
+print("*******************")
+print("Robot is in position:")
+print(desired_p)
+print("And orientation:")
 print(current_o)
 
 
